0a2bc7492dd95d5a2c062ef0ffb6c0c9f12e087e
bryanruuiz <bruiz4060@gmail.com>
2025-05-03 14:41:39 -0500

    changes to merge with master

---
+++ Employee.py
@@ -0,0 +1,6 @@
+class Employee:
+    def __init__(self, id: int, first_name: str, last_name: str, salary : int):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
